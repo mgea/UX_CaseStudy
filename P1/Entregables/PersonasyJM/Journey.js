2f2f8433001563bef0b559e03e62e8465e88a29c
Naroa Alonso Fernández <46995761+NaroaAlonso@users.noreply.github.com>
2020-02-25 16:48:03 +0100

    Add files via upload

---
+++ P1/Entregables/PersonasyJM/Journey.js
@@ -0,0 +1,151 @@
+/*******************************************/
+/*             JOURNEY.JS                  */
+/*     Datos para USER JOURNEY MAP         */   
+/*          [DIU] UX Toolkit               */                        
+/*          ver 1.0, 2019                  */
+/*******************************************/
+    
+/****  README:       */
+/****  Modifica los datos para los Journey Map (uno para cada Persona)  */
+/****  Usa los 6 pasos y sigue las instrucciones */   
+/****  Las imagenes para  'Photo', 'feelX', 'imaX' están en carpeta ./photos **/
+/****  Si se usan nuevas imágenes se deben añadir a esa carpeta **/
+/****  Los valores de rating están entre 1..5 **/
+/****  recursos de imágenes:  https://www.vectorstock.com/royalty-free-vectors/vectors-by_zdeneksasek ***/
+
+
+
+
+angular.module("angular", [])
+	.controller("controller", ["$scope", function($scope) { 
+		
+		$scope.JourneyIndex = 0;
+        
+        $scope.Journeys = [
+			{		
+                
+                /*************************************/
+                /**** PRIMER USER JOURNEY MAP  *******/
+                /*** Cambiar datos             *******/
+                /*************************************/
+                
+				Id: 0,
+				Name: "Juan Lopez",
+                Photo: "man.png",
+    
+                /*** PASO #1: INSPIRACION ***/ 
+                goal1: "quiere preparar un viaje con sus amigos en Semana Santa",
+                touch1: "agenda",
+                feel1: "4",
+                con1: "ver cuantos días puede tener libres para organizar lugar de viaje ",
+                ima1: "cartoon-planning.png",
+				
+                /*** PASO #2: DECICION ***/ 
+                goal2: "Busca en internet ofertas para esas fechas",
+                touch2: "ordenador",
+                feel2: "2",
+                con2: "hay demasiada información y pierde mucho tiempo, no hay precios 'baratos'",
+                ima2: "cartoon-PCangry.png",
+                
+                /*** PASO #3: ACTUA ***/ 
+                
+                goal3: "Decide buscar un alojamiento rural  en plasencia, donde hay procesiones y parece que hará buen tiempo",
+                touch3: "móvil (el tiempo)",
+                feel3: "3",
+                con3: "Está preocupado por el tiempo y el desplazamiento (coche y aparcamiento)",
+                ima3: "cartoon-phone.png",
+                
+                /*** PASO #4: OBSERVA ***/ 
+                
+                goal4: "Los amigos le recomiendan una página para escoger alojamientos",
+                touch4: "ordenador",
+                feel4: "4",
+                con4: "Buscar opciones en el lugar que había seleccionado, viendo precios y distancias, tiene que ver si hay aparcamiento fácil",
+                ima4: "cartoon-PCtyping.png",
+                
+                 /*** PASO #5: ANALIZA ***/ 
+                
+                goal5: "Se encuentra 3 opciones que encajan en sus preferencias",
+                touch5: "móvil (whatsapp)",
+                feel5: "2",
+                con5: "Llama a sus amigos (whatsapp no responen) para ver cual es su preferencia, tienen que reservar rápido por los precios",
+                ima5: "cartoon-phoning.png",
+                
+                
+                /*** PASO #6: CONCLUSION ***/ 
+                
+                goal6: "Consigue reservar, otro año se encarga otro!",
+                touch6: "ordenador",
+                feel6: "3",
+                con6: "algunos amigos no confirmaron por lo que tuvo que seleccionar reserva con posibilidad de cancelación",
+                ima6: "cartoon-resting.png",
+                
+			},
+			{	
+                /*************************************/
+                /**** SEGUNDO USER JOURNEY MAP *******/
+                /***      Cambiar datos        *******/
+                /*************************************/
+                
+				Id: 1,
+				Name: "Monica Suarez",
+                Photo: "woman.png",
+                
+				 /*** PASO #1: INSPIRACION ***/ 
+                goal1: "Quiere preparar un viaje con su familia para Verano, tiene sólo 15 dias libres",
+                touch1: "agenda",
+                feel1: "5",
+                con1: "Quiere ir a un pais exotico pero que tenga atracciones para niños pequeños",
+                ima1: "cartoon-going.png",
+                
+                /*** PASO #2: DECICION ***/ 
+                goal2: "Ir a una agencia de viajes, y decirle sus preferencias y planes",
+                touch2: "Servicio (agencia)",
+                feel2: "4",
+                con2: "Tiene que desplazarse a agencia, explica su intenciones, le llamaran porque no hay nada interesante",
+                ima2: "cartoon-teamthinking.png",
+                
+                /*** PASO #3: ACTUA ***/ 
+                
+                goal3: "Le llaman a los pocos días con un viaje que no le convence",
+                touch3: "Móvil (llamada)",
+                feel3: "2",
+                con3: "Piensa que ha perdido el tiempo",
+                ima3: "cartoon-phoningangry.png",
+                
+                /*** PASO #4: OBSERVA ***/ 
+                
+                goal4: "Busca una oferta en hoteles cerca de playa y con parque atracciones",
+                touch4: "Móvil (webapp)",
+                feel4: "2",
+                con4: "No hay mucha información del alojamiento ni de lo que hay alrededor, aunque el precio está bien, va por la calle por lo que está incómoda",
+                ima4: "cartoon-phone-street.png",
+                
+                 /*** PASO #5: ANALIZA ***/ 
+                
+                goal5: "Reserva a traves de la aplicación ",
+                touch5: "Móvil (webapp)",
+                feel5: "3",
+                con5: "Le pide muchos datos y le resulta incómodo completar formulario",
+                ima5: "cartoon-phone-sitting.png",
+
+                
+                /*** PASO #6: CONCLUSION ***/ 
+                
+                goal6: "Consiguie reservar para vacaciones pero no era lo que tenía en mente",
+                touch6: "Ordenador (reserva OK)",
+                feel6: "2",
+                con6: "Tendrá que buscar más información del lugar para ver que actividades ofrece y donde aparacar!",
+                ima6: "cartoon-PChard.png",
+                
+                
+                
+			}
+		];
+        
+		$scope.model = $scope.Journeys[0];
+
+	}])
+
+
+
---
+++ P1/Entregables/PersonasyJM/README.md
@@ -0,0 +1,37 @@
+# UX DIU Toolkit
+
+Herramientas en HTML5 para creación de: 
+    - 2 Personas 
+    - 2 User Journey Map 
+    
+Version online: https://mgea.github.io/UX-DIU-Toolkit/index.html
+Repositorio en: https://github.com/mgea/UX-DIU-Toolkit
+
+V.1.0 Feb,12 2020
+
+
+Se deben crear *dos personas ficticias*. (http://www.uxforthemasses.com/personas/) que puedan ser usuarios interesados en las actividades que se ofertan en esa ciudad. Información acerca de cómo construir Personas Ficticias: http://www.ux-lady.com/diy-user-personas/. Hay herramientas (fremium) para crear Personas ficticias: https://xtensio.com/how-to-create-a-persona/. Con https://randomuser.me podemos  obtener imágenes/features de personas aleatorias.
+
+
+Incluir *dos mapas de experiencia del usuario* (User Journey Experience Map) uno para cada usuario donde se describa la interacción de un usuarios con el interfaz para realizar una tarea.  Más información de cómo abordar una mapa de experiencia de usuario: http://theuxreview.co.uk/user-journeys-beginners-guide/. Se puede usar la herramienta online: https://uxpressia.com/ o bien la plantilla http://www.burokoos.com/assets/uploads/2012/04/koos_customer_journey_mapping.pdf 
+https://uxpressia.com/templates/cjm-for-website-visitor 
+
+
+Alternativas:
+
+- editor online XTENSIO para crear Personas ficticias: https://xtensio.com/how-to-create-a-persona/
+- herramienta online: https://uxpressia.com/
+- plantillas: 
+
+
+Tareas: 
+
+ - Incluir información en ficheros personas.js / Journey.js
+ - Preparado para 2 plantillas
+ - Se debe añadir información del equipo
+
+ToDo: 
+ - Configurar al movil
+ - E/S con JSON
+
+
